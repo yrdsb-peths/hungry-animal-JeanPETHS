fe7e921c89a50c0679130c3c42607d0b00add411
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-11-27 13:32:18 -0500

    Try AddedToWorld

--- Elephant.java
+++ Elephant.java
@@ -38,6 +38,10 @@ public class Elephant extends Actor
         setImage(idleRight[0]);
     }
     
+    public void addedToWorld(World w) {
+        this.world = (MyWorld) getWorld();
+    }
+    
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
